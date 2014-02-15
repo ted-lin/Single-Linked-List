581d182f450d80cf5edef39ea3f92200eea91286
ted_lin <ted_lin@asus.com>
2014-02-15 20:21:07 +0800

    list: add init.h
    add type define, api, macro

---
+++ init.h
@@ -0,0 +1,36 @@
+#ifndef __ALGO_LIST_H__
+#define __ALGO_LIST_H__
+
+#include <stdlib.h>
+
+typedef struct ListElmt_ {
+  void             *data;
+  struct ListElmt_ *next;
+} ListElmt;
+
+typedef struct List_ {
+  int      size;
+  int      (*match)(const void *key1, const void *key2);
+  void     (*destroy)(void *data);
+
+  ListElmt *head;
+  ListElmt *tail;
+} List;
+
+/* generic interface */
+void list_init(List *list, void (*destroy)(void *data));
+void list_destroy(List *list);
+
+int list_ins_next(List *list, ListElmt *element, const void *data);
+int list_rem_next(List *list, ListElmt *element, void **data);
+
+/* generic macro */
+#define list_size(list) ((list)->size)
+#define list_head(list) ((list)->head)
+#define list_tail(list) ((list)->tail)
+#define list_is_head(list, element) ((element) == (list)->head ? 1 : 0)
+#define list_is_tail(list, element) ((element) == (list)->tail ? 1 : 0)
+#define list_data(element) ((element)->data)
+#define list_next(element) ((element)->next)
+
+#endif /* end define of __ALGO_LIST_H__ */
